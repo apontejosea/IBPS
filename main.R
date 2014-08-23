135cf9420dc063da0315956d4924b229b8f03c66
xjosianx@gmail.com <xjosianx@gmail.com>
2014-08-23 12:07:29 -0400

    Plenty of development, but incomplete

--- main.R
+++ main.R
@@ -1,17 +1,44 @@
-# TODO: remove defaults at the end
-generate_lm  <- function(data_file='audit.csv', 
-                         fieldnames=list(x=c('Age', 'Employment', 
-                                             'Gender', 'Income'),
-                                         y='Hours')) {
+r2c  <- function(x) {
+  type_pairs <- list(C=c('int',     'double',  'char *',    'bool',    'char *'),
+                     R=c('integer', 'numeric', 'character', 'logical', 'factor'))
+  type_pairs$C[type_pairs$R == x]
+}
 
-  #TODO: Ensure parameters are well formed. E.g. number of vars
-  txt_formula<- paste(fieldnames$y, '~', paste0(fieldnames$x, collapse='+'))
-  input_data <- read.csv(data_file)
-  lm(txt_formula, data=input_data)
+c_type <- function(.df) {
+  unlist(lapply(.df, function(x) r2c(class(x))))
+}
 
-  # Generate model output
-  #TODO: file names should be dynamic
-  sink('model_output.txt')
+#TODO: ...
+generate_c_formula <- function(x, lm_fit, coef, .data)  {
+  "x"
+}
+
+parameters_string <- function(field_names, .data) {
+  var_names  <- unlist(c(field_names$y, field_names$x))
+  types      <- c_type(.data[1, var_names])
+  c_params   <- paste0(types, ' ', tolower(var_names))
+  parameters <- paste(c_params, collapse=', ')
+  parameters
+}
+
+generate_c_file <- function(field_names, lm_fit) {
+  file_name <- paste0('model_', tolower(field_names$y), '.c')
+  param_str <- parameters_string(field_names, .data)
+  c_formula <- with(field_names, generate_c_formula(x, y))
+  contrasts <- names(attr(qr(lm_fit)$qr, 'contrasts'))
+  print(param_str)
+}
+
+#TODO: use this function to generate clean coefficients
+generate_coef <- function(lm_fit) {
+  .coef <- coef(lm_fit)
+}
+
+#TODO: file names should be dynamic
+#TODO: make function name consistent with others
+print_model_output <- function(lm_fit, 
+                               model_output_file='model_output.txt') {
+  sink(model_output_file)
   cat('COEFFICIENTS:\n')
   cat(paste0(coef(lm_fit), sep='\n'))
   cat('\n\n')
@@ -20,10 +47,44 @@ generate_lm  <- function(data_file='audit.csv',
   cat('SUMMARY:\n')
   print(summary(lm_fit))
   sink()
-
-  # Generate C routine
-  #TODO: file names should be dynamic
-  sink('routine.c')
-  
-  sink()
 }
+
+# TODO: remove defaults at the end
+fit_lm  <- function(.data, 
+                    field_names=list(x=c('Age', 'Employment',
+                                        'Gender', 'Income'), y='Hours')) {
+  txt_formula<- paste(field_names$y, '~',
+                      paste0(field_names$x, collapse='*'))
+  return(lm(txt_formula, data=.data, qr=T))
+}
+
+generate_model_routine <- function(data_file, field_names) {
+  .data  <- read.csv(data_file, stringsAsFactors = T)
+  lm_fit <- fit_lm(.data, field_names)
+  browser()
+  print_model_output(lm_fit)
+  generate_c_file(field_names, lm_fit)
+  cat('DONE\n')
+}
+
+#TODO: Any preferred way to name the C routines?
+run <- function() {
+  # to enable afterwards
+  #   model_par_sets 
+  #     <- list(A=list(x=c('Age', 'Employment', 'Gender', 'Income'), 
+  #                    y='Hours'),
+  #          B=list(x=c('Age','Marital','Occupation', 'Education','Hours'), 
+  #                    y='Income'))
+  # 
+  model_par_sets <- 
+    list(A=list(x=c('Age', 'Employment', 'Gender', 'Income'), y='Hours'))
+
+  for(i in length(model_par_sets)) {
+    generate_model_routine(data_file='audit.csv', 
+                           field_names=model_par_sets[[i]])
+  }
+}
+
+run()
+
+Income ~ Age ~ Marital Occupation', 'Education','Hours'
--- main.c
+++ main.c
@@ -1,18 +1,29 @@
 /* Hello World program */
-
 #include<stdio.h>
+#include<string.h>
 
-main() {
 
-  
-
-/* 
+/******************************* 
  * Determine number of fields
  * 
  *
- */
+ *******************************/
+double model_asset_hours(int age, char * employment, 
+                         char * gender, double income) {
 
+  int const EMPLOYMENT_SIZE = 10;
+  int const GENDER_SIZE = 2;
 
+  int employment_index;
+  int gender_index;
+
+  double coef[EMPLOYMENT_SIZE][GENDER_SIZE];
+
+  printf("%6.9f", 
+      (double)age + (double)income);
 }
 
-
+//TODO: Check that the R model is consistent with C routines
+void main() {
+  model_asset_hours(6.0, "Unemployed", "FEMALE", 60050);
+}
